11f4a69921adebfceee12a81b0d91158cfc14ecb
Mark-Cyber0628 <szakalmark0628@gmail.com>
2025-07-29 14:56:49 +0200

    vite.config.js base

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/A_TE_REPO_NEVE",
+  base: "/Learning_react/",
   plugins: [react()],
 })
